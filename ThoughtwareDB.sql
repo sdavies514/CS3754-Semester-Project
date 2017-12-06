2c5516cd3d1dd0e8e6db0d436f8f545961c63937
Kyle Dyess <kdyess97@vt.edu>
2017-12-06 15:22:42 -0500

    Fixed SQL syntax error, extra ',' caused most table generation to fail.

--- ThoughtwareDB.sql
+++ ThoughtwareDB.sql
@@ -55,7 +55,7 @@ CREATE TABLE Project
 (
     id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
     name VARCHAR (256) NOT NULL,
-    hashed_password VARCHAR (64) NOT NULL,
+    hashed_password VARCHAR (64) NOT NULL
 );
 
 DELIMITER $$
@@ -180,6 +180,16 @@ Create Table ProjectFile
     FOREIGN KEY (project_id) REFERENCES Project(id) ON DELETE CASCADE
 );
 
+Create Table Activity
+(
+    id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    type VARCHAR (256) NOT NULL,
+    message VARCHAR (256) NOT NULL,
+    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    project_id INT UNSIGNED,
+    FOREIGN KEY (project_id) REFERENCES Project(id) ON DELETE CASCADE
+);
+
 DELIMITER $$
 CREATE TRIGGER `project_file_after_insert` AFTER INSERT
 	ON `ProjectFile`
@@ -230,16 +240,6 @@ CREATE TRIGGER `project_file_after_delete` AFTER DELETE
 		);
     END$$
 DELIMITER ;
-
-Create Table Activity
-(
-    id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
-    type VARCHAR (256) NOT NULL,
-    message VARCHAR (256) NOT NULL,
-    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    project_id INT UNSIGNED,
-    FOREIGN KEY (project_id) REFERENCES Project(id) ON DELETE CASCADE
-);
 
 Create Table Milestone
 (
