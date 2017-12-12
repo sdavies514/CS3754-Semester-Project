730048ea0bb631e163aabb28198800497bd3bcb8
TJ Corley <tjcorley30@hotmail.com>
2017-12-12 13:31:01 -0500

    Add chat support for projects

--- src/java/com/mycompany/chat/MessageDecoder.java
+++ src/java/com/mycompany/chat/MessageDecoder.java
@@ -3,18 +3,18 @@ package com.mycompany.chat;
 import org.primefaces.push.Decoder;
  
 /**
- * A Simple {@link org.primefaces.push.Decoder} that decode a String into a {@link Message} object.
+ * A Simple {@link org.primefaces.push.Decoder} that decode a String into a {@link ChatMessage} object.
  */
-public class MessageDecoder implements Decoder<String,Message> {
+public class MessageDecoder implements Decoder<String,ChatMessage> {
  
     //@Override
-    public Message decode(String s) {
+    public ChatMessage decode(String s) {
         String[] userAndMessage = s.split(":");
         if (userAndMessage.length >= 2) {
-            return new Message().setUser(userAndMessage[0]).setText(userAndMessage[1]);
+            return new ChatMessage().setUser(userAndMessage[0]).setText(userAndMessage[1]);
         } 
         else {
-            return new Message(s);
+            return new ChatMessage(s);
         }
     }
 }
