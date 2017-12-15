ff545b9958c981fcbfa080e4564cc6fa6ce5a8a5
Casey Jordan Butenhoff <eolxe@vt.edu>
2017-12-14 20:57:11 -0500

    Remove hack from presentation data SQL script

--- PresentationData.sql
+++ PresentationData.sql
@@ -1,6 +1,3 @@
-INSERT INTO thoughtwaredb.`user` (username, hashed_password, first_name, middle_name, last_name, address1, address2, city, `state`, zipcode, security_question, security_answer, email, google_image_url)
-VALUES ('sdavies514@gmail.com', '$2a$10$xIWSEe2Nq/SvU0vPhW8H8uYQwXp5NuAhHkOAoFMGL65TkodEVALva', 'Shane', NULL, 'Davies', 'Google account, please update!', NULL, 'Google account, please update!', 'AK', '00000', 0, 'Google account, please update!', 'sdavies514@gmail.com', 'https://lh5.googleusercontent.com/-_kILPwreOCg/AAAAAAAAAAI/AAAAAAAAAAA/AFiYof0QLshiPkCt6BjnfteHDL86Edeh_Q/s96-c/photo.jpg');
-
 INSERT INTO `Activity` (
     type,
     message,
